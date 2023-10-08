c6f5edfa653899b2b6ed4f701965a619141afddb
syf20020816 <syf20020816@outlook.com>
2023-10-09 02:25:39 +0800

    20231009-1-45%

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,10 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: u32) -> u32 {
+    return if 40.lt(&num) { num } else { num * 2 };
+}
 
 // Don't modify this function!
 #[test]
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,11 +30,17 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let res = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(num) => format!("{}{}", string, "bar".repeat(*num)),
+            };
+            output.push(res)
         }
         output
     }
@@ -45,8 +49,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
